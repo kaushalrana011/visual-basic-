51ecfd3dd2cec46ba45ced9ad6c3bdf1c422509e
Kaushal Rana <kaushalrana5160@gmail.com>
2026-08-11 12:28:39 +0530

    Add files via upload

---
+++ Recursion/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label1.Location = New Point(89, 98)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(202, 38)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter Number"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(334, 98)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(157, 175)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 2
+        Button1.Text = "Factorial"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(334, 175)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 34)
+        Button2.TabIndex = 3
+        Button2.Text = "Fibonacci"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+
+End Class
---
+++ Recursion/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+    Function Fact(n As Integer) As Integer
+        If n = 0 Then
+            Return 1
+        Else
+            Return n * Fact(n - 1)
+        End If
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer
+        n = TextBox1.Text
+        MsgBox("Factorial = " & Fact(n))
+    End Sub
+    Function Fib(n As Integer) As Integer
+        If n = 0 Then
+            Return 0
+        ElseIf n = 1 Then
+            Return 1
+        Else
+            Return Fib(n - 1) + Fib(n - 2)
+        End If
+    End Function
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim n As Integer
+        Dim i As Integer
+        Dim s As String = ""
+
+        n = TextBox1.Text
+        For i = 0 To n - 1
+            s = s & Fib(i) & " "
+        Next
+        MsgBox("Fibonacci Series :" & vbCrLf & s)
+
+    End Sub
+    End Class
