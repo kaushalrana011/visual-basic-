0496cbf4d0f1716ec5dccffb91fbc05bad8340a4
Kaushal Rana <kaushalrana5160@gmail.com>
2026-08-11 12:43:48 +0530

    Add files via upload

---
+++ Overload/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim s As New Shape
+        s.Area(TextBox1.Text, TextBox2.Text)
+        s.Area(TextBox3.Text)
+    End Sub
+End Class
---
+++ Overload/My Project/Shape.vb
@@ -0,0 +1,9 @@
+﻿Public Class Shape
+    Public Sub Area(length As Double, breadth As Double)
+        MsgBox("Area of Rectangle = " & (length * breadth))
+    End Sub
+    Public Sub Area(side As Double)
+        MsgBox("Area of Square = " & (side * side))
+    End Sub
+
+End Class
