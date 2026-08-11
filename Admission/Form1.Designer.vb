491c33670559696fa4273f7bc6d1819746408e69
Kaushal Rana <kaushalrana5160@gmail.com>
2026-08-11 12:50:59 +0530

    Add files via upload

---
+++ Admission/Form1.Designer.vb
@@ -0,0 +1,452 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        DateTimePicker1 = New DateTimePicker()
+        ComboBox1 = New ComboBox()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        Button1 = New Button()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        CheckBox4 = New CheckBox()
+        Label13 = New Label()
+        GroupBox1 = New GroupBox()
+        RadioButton4 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        GroupBox2 = New GroupBox()
+        RadioButton5 = New RadioButton()
+        RadioButton6 = New RadioButton()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Segoe UI", 23F, FontStyle.Bold)
+        Label1.ForeColor = SystemColors.MenuHighlight
+        Label1.Location = New Point(364, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(376, 62)
+        Label1.TabIndex = 0
+        Label1.Text = "Admission Form"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 17F, FontStyle.Bold Or FontStyle.Underline)
+        Label2.Location = New Point(164, 95)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(263, 46)
+        Label2.TabIndex = 1
+        Label2.Text = "Student Details"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label3.Location = New Point(164, 174)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(206, 38)
+        Label3.TabIndex = 2
+        Label3.Text = "Student Name"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label4.Location = New Point(164, 231)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(205, 38)
+        Label4.TabIndex = 3
+        Label4.Text = "Father's Name"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label5.Location = New Point(164, 344)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(216, 38)
+        Label5.TabIndex = 5
+        Label5.Text = "Phone Number"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label6.Location = New Point(164, 287)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(220, 38)
+        Label6.TabIndex = 4
+        Label6.Text = "Mother's Name"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 17F, FontStyle.Bold Or FontStyle.Underline)
+        Label7.Location = New Point(164, 687)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(157, 46)
+        Label7.TabIndex = 9
+        Label7.Text = "Facilities"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label8.Location = New Point(164, 514)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(112, 38)
+        Label8.TabIndex = 8
+        Label8.Text = "Gender"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label9.Location = New Point(164, 458)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(106, 38)
+        Label9.TabIndex = 7
+        Label9.Text = "Course"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label10.Location = New Point(164, 401)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(189, 38)
+        Label10.TabIndex = 6
+        Label10.Text = "Date of Birth"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label11.Location = New Point(164, 757)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(164, 38)
+        Label11.TabIndex = 10
+        Label11.Text = "Bus Facility"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label12.Location = New Point(654, 744)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(201, 38)
+        Label12.TabIndex = 11
+        Label12.Text = "Hostel Facility"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(555, 182)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 12
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(555, 231)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 13
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(555, 294)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(150, 31)
+        TextBox3.TabIndex = 14
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(555, 351)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(150, 31)
+        TextBox4.TabIndex = 15
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(555, 407)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(300, 31)
+        DateTimePicker1.TabIndex = 16
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"BCA", "BSC-IT", "BBA", "AGRICULTURE", "BFA", "MCA", "MSC-IT", "MFA", "OTHERS"})
+        ComboBox1.Location = New Point(555, 466)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(219, 33)
+        ComboBox1.TabIndex = 17
+        ComboBox1.Text = "----SELECT COURSE----"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(555, 514)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(75, 29)
+        RadioButton1.TabIndex = 18
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Male"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(647, 514)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(93, 29)
+        RadioButton2.TabIndex = 19
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Female"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ControlLightLight
+        Button1.Font = New Font("Segoe UI", 13F, FontStyle.Bold)
+        Button1.Location = New Point(923, 929)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(152, 46)
+        Button1.TabIndex = 21
+        Button1.Text = "Submit"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(184, 631)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(74, 29)
+        CheckBox1.TabIndex = 22
+        CheckBox1.Text = "10th"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(364, 631)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(74, 29)
+        CheckBox2.TabIndex = 23
+        CheckBox2.Text = "12th"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(767, 631)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(60, 29)
+        CheckBox3.TabIndex = 25
+        CheckBox3.Text = "PG"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.Location = New Point(555, 631)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(62, 29)
+        CheckBox4.TabIndex = 24
+        CheckBox4.Text = "UG"
+        CheckBox4.UseVisualStyleBackColor = True
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Font = New Font("Segoe UI", 15F, FontStyle.Bold)
+        Label13.Location = New Point(164, 583)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(441, 41)
+        Label13.TabIndex = 26
+        Label13.Text = "Select Education Qualification"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton4)
+        GroupBox1.Controls.Add(RadioButton3)
+        GroupBox1.Location = New Point(164, 804)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(300, 150)
+        GroupBox1.TabIndex = 31
+        GroupBox1.TabStop = False
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.Location = New Point(92, 30)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(71, 29)
+        RadioButton4.TabIndex = 1
+        RadioButton4.TabStop = True
+        RadioButton4.Text = "  No"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(11, 30)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(72, 29)
+        RadioButton3.TabIndex = 0
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "  Yes"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(RadioButton5)
+        GroupBox2.Controls.Add(RadioButton6)
+        GroupBox2.Location = New Point(647, 799)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(300, 150)
+        GroupBox2.TabIndex = 32
+        GroupBox2.TabStop = False
+        ' 
+        ' RadioButton5
+        ' 
+        RadioButton5.AutoSize = True
+        RadioButton5.Location = New Point(92, 30)
+        RadioButton5.Name = "RadioButton5"
+        RadioButton5.Size = New Size(71, 29)
+        RadioButton5.TabIndex = 1
+        RadioButton5.TabStop = True
+        RadioButton5.Text = "  No"
+        RadioButton5.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton6
+        ' 
+        RadioButton6.AutoSize = True
+        RadioButton6.Location = New Point(11, 30)
+        RadioButton6.Name = "RadioButton6"
+        RadioButton6.Size = New Size(72, 29)
+        RadioButton6.TabIndex = 0
+        RadioButton6.TabStop = True
+        RadioButton6.Text = "  Yes"
+        RadioButton6.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ButtonHighlight
+        ClientSize = New Size(1097, 997)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label13)
+        Controls.Add(CheckBox3)
+        Controls.Add(CheckBox4)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(Button1)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(ComboBox1)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label7)
+        Controls.Add(Label8)
+        Controls.Add(Label9)
+        Controls.Add(Label10)
+        Controls.Add(Label5)
+        Controls.Add(Label6)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+
+End Class
---
+++ Admission/Form1.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Button
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim studentName As String = TextBox1.Text
+        Dim fatherName As String = TextBox2.Text
+        Dim motherName As String = TextBox3.Text
+        Dim phoneNumber As String = TextBox4.Text
+        Dim dob As String = DateTimePicker1.Value.ToShortDateString()
+        Dim course As String = ComboBox1.Text
+
+        Dim gender As String = ""
+        If RadioButton1.Checked Then
+            gender = "Male"
+        ElseIf RadioButton2.Checked Then
+            gender = "Female"
+        End If
+
+        Dim edu As String = ""
+        If CheckBox1.Checked Then edu += "10th "
+        If CheckBox2.Checked Then edu += "12th "
+        If CheckBox3.Checked Then edu += "UG "
+        If CheckBox4.Checked Then edu += "PG"
+
+        Dim bus As String = ""
+        If RadioButton3.Checked Then bus = "Yes" Else bus = "No"
+
+        Dim hostel As String = ""
+        If RadioButton5.Checked Then hostel = "Yes" Else hostel = "No"
+
+        Dim info As String = "Student Name: " & studentName & vbCrLf &
+                             "Father's Name: " & fatherName & vbCrLf &
+                             "Mother's Name: " & motherName & vbCrLf &
+                             "Phone Number: " & phoneNumber & vbCrLf &
+                             "Date of Birth: " & dob & vbCrLf &
+                             "Course: " & course & vbCrLf &
+                             "Gender: " & gender & vbCrLf &
+                             "Education: " & edu & vbCrLf &
+                             "Bus Facility: " & bus & vbCrLf &
+                             "Hostel Facility: " & hostel
+
+        MessageBox.Show(info, "Submitted Information")
+    End Sub
+
+End Class
