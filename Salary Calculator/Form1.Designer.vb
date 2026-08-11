cb3a7190e4d8ef7327d69b6bf553e13dd449509b
Kaushal Rana <kaushalrana5160@gmail.com>
2026-08-11 12:27:09 +0530

    Add files via upload

---
+++ Salary Calculator/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 20F, FontStyle.Bold)
+        Label1.Location = New Point(95, 35)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(536, 54)
+        Label1.TabIndex = 0
+        Label1.Text = "Employee Salary Calculator"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label2.Location = New Point(134, 161)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(172, 38)
+        Label2.TabIndex = 1
+        Label2.Text = "Basic Salary"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(390, 169)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button1.Location = New Point(292, 263)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(143, 47)
+        Button1.TabIndex = 3
+        Button1.Text = "Calculate"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1366, 934)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Salary Calculator/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim basic, gross, tax, pf, net As Double
+        basic = TextBox1.Text
+        gross = basic + (basic * 20 / 100)
+        tax = gross * 10 / 100
+        pf = basic * 12 / 100
+        net = gross - tax - pf
+        MsgBox("Gross Salary = " & gross & vbCrLf &
+               "Tax = " & tax & vbCrLf &
+               "PF = " & pf & vbCrLf &
+               "Net Salary = " & net)
+    End Sub
+End Class
