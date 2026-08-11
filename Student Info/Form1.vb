8dafddc1498027b70d6d75b560064b7090dbc410
Kaushal Rana <kaushalrana5160@gmail.com>
2026-08-11 12:23:57 +0530

    Add files via upload

---
+++ Student Info/Form1.vb
@@ -0,0 +1,11 @@
+﻿
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim s As New Student
+        s.GetData(TextBox1.Text,
+                  TextBox2.Text,
+                  TextBox3.Text)
+        s.ShowData()
+    End Sub
+End Class
---
+++ Student Info/Student.vb
@@ -0,0 +1,15 @@
+﻿Public Class Student
+    Dim RollNo As Integer
+    Dim Name As String
+    Dim Course As String
+    Public Sub GetData(r As Integer, n As String, c As String)
+        RollNo = r
+        Name = n
+        Course = c
+    End Sub
+    Public Sub ShowData()
+        MsgBox("Roll No : " & RollNo & vbCrLf &
+               "Name : " & Name & vbCrLf &
+               "Course : " & Course)
+    End Sub
+End Class
