7bc6ef4dda1a3f3376719fdeb04359320164c6b3
Kaushal Rana <kaushalrana5160@gmail.com>
2026-08-11 12:49:01 +0530

    Add files via upload

---
+++ Clock/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        UpdateClockDisplay()
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+            UpdateClockDisplay()
+        End Sub
+
+        Private Sub UpdateClockDisplay()
+        Label1.Text = DateTime.Now.ToString("hh:mm:ss tt")
+    End Sub
+
+    End Class
+
+
